2f4da5d4e114e8cb4f8a34f698f16660e32c6e64
El Diablo <eldiablolives@live.com>
2020-05-08 16:39:53 +0700

    adjust

--- button/button.module.ts
+++ button/button.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MgButtonComponent } from './button.component';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
 
 const components = [MgButtonComponent];
 
