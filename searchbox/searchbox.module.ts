baee19315f8b38490afeec4b7869b9450b187967
El Diablo <eldiablolives@live.com>
2020-05-09 18:18:07 +0700

    fixex

--- searchbox/searchbox.module.ts
+++ searchbox/searchbox.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MgSearchboxComponent } from './searchbox.component';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
 
 const components = [MgSearchboxComponent];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    MatButtonModule
-  ],
+  imports: [CommonModule, MatButtonModule],
   declarations: components,
-  exports: components
+  exports: components,
 })
-export class MgSearchboxModule { }
+export class MgSearchboxModule {}
--- tree/tree.module.ts
+++ tree/tree.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { MgTreeComponent } from "./tree.component";
-import { MatButtonModule } from "@angular/material";
-import { MgTreeItemComponent } from "./tree-item.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MgTreeComponent } from './tree.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MgTreeItemComponent } from './tree-item.component';
 
 const components = [MgTreeComponent, MgTreeItemComponent];
 
 @NgModule({
   imports: [CommonModule, MatButtonModule],
   declarations: components,
-  exports: components
+  exports: components,
 })
 export class MgTreeModule {}
