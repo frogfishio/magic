e5ecacd29debb078d5166a4a4384fbdced4e0a02
El Diablo <eldiablolives@live.com>
2020-05-12 21:38:17 +0700

    fixex

--- button-bar/button-bar.module.ts
+++ button-bar/button-bar.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { MgButtonBarComponent } from "./button-bar.component";
-import { MgButtonBarButtonComponent } from "./button-bar-button.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MgButtonBarComponent } from './button-bar.component';
+import { MgButtonBarButtonComponent } from './button-bar-button.component';
 
 const components = [MgButtonBarComponent, MgButtonBarButtonComponent];
 
 @NgModule({
   imports: [CommonModule],
   declarations: components,
-  exports: components
+  exports: components,
 })
 export class MgButtonBarModule {}
