5301661bb2dcb00812f6fb1b3f3c2005a7c0b885
el'diablo <eldiablolives@live.com>
2021-06-25 21:39:43 +0700

    refactor

--- button/button.component.ts
+++ button/button.component.ts
@@ -7,35 +7,37 @@ import { Router } from '@angular/router';
   styleUrls: ['./button.component.sass'],
 })
 export class MgButtonComponent {
-  // @Input() navigate?: string;
-  // @Input() action?: () => null;
-  // @Input() set class(value: string) {
-  //   this._class = value;
-  // }
-  // get class() {
-  //   return this._class || 'default';
-  // }
-  // private _class = 'default';
-  // processing?: any;
-  // constructor(private router: Router) {}
-  // buttonClicked() {
-  //   if (this.navigate) {
-  //     this.router.navigateByUrl(this.navigate);
-  //   } else if (this.action) {
-  //     this.processing = true;
-  //     const promise = this.action();
-  //     if (!promise || !(promise instanceof Promise)) {
-  //       this.processing = false;
-  //     } else {
-  //       promise
-  //         .then(() => {
-  //           this.processing = false;
-  //         })
-  //         .catch((err) => {
-  //           this.processing = false;
-  //           console.log(err);
-  //         });
-  //     }
-  //   }
-  // }
+  @Input() navigate?: string;
+  @Input() action?: () => any;
+  @Input() set class(value: string) {
+    this._class = value;
+  }
+  get class() {
+    return this._class || 'default';
+  }
+  private _class = 'default';
+  processing = false;
+
+  constructor(private router: Router) {}
+
+  async buttonClicked() {
+    if (this.navigate) {
+      this.router.navigateByUrl(this.navigate);
+    } else if (this.action) {
+      this.processing = true;
+      const promise = this.action();
+      if (!promise || !(promise instanceof Promise)) {
+        this.processing = false;
+      } else {
+        promise
+          .then(() => {
+            this.processing = false;
+          })
+          .catch((err) => {
+            this.processing = false;
+            console.log(err);
+          });
+      }
+    }
+  }
 }
