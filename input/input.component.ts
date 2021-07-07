7872c3822370f61e850968990d16153bdccd992f
el diabo <eldiablolives@live.com>
2021-07-07 14:09:44 +0700

    improvements

--- input/input.component.ts
+++ input/input.component.ts
@@ -11,17 +11,13 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
       multi: true,
       useExisting: MgInputComponent,
     },
-    // {
-    //   provide: NG_VALIDATORS,
-    //   multi: true,
-    //   useExisting: ChooseQuantityComponent
-    // }
   ],
 })
 export class MgInputComponent implements ControlValueAccessor {
   @Input() type = 'text';
   @Input() placeholder = '';
   @Input() set value(val: any) {
+    console.log(`set -> ${val}`);
     this._value = val;
     this.onChange(val);
   }
@@ -30,12 +26,25 @@ export class MgInputComponent implements ControlValueAccessor {
     return this._value;
   }
 
+  @Input() onValue?: string;
+  @Input() offValue?: string;
+
   private _value?: any;
 
-  onChange: any = () => {};
-  onTouch: any = () => {};
+  onChange: any = () => { };
+  onTouch: any = () => { };
 
-  constructor() {}
+  constructor() { }
+
+  toggle() {
+    console.log(`VAL = [${this.value}]`);
+    if (this.value === this.onValue) {
+      this.value = this.offValue;
+    } else {
+      this.value = this.onValue;
+    }
+  }
+
   writeValue(obj: any): void {
     this.value = obj;
     // this.onChange(obj);
@@ -46,6 +55,4 @@ export class MgInputComponent implements ControlValueAccessor {
   registerOnTouched(fn: any): void {
     this.onTouch = fn;
   }
-
-  // ngOnInit(): void {}
 }
--- modal/modal.service.ts
+++ modal/modal.service.ts
@@ -1,11 +1,12 @@
 import { ApplicationRef, ComponentFactoryResolver, Injectable, ViewContainerRef } from '@angular/core';
 import { AppComponent } from 'src/app/app.component';
-import { MgModalComponent } from './modal.component';
 
 @Injectable({ providedIn: 'root' })
 export class MgModalService {
   private _viewContainerRef?: ViewContainerRef;
   private _active?: any;
+
+  private _onClose?: () => any;
 
   constructor(private appRef: ApplicationRef, private componentFactoryResolver: ComponentFactoryResolver) { }
 
@@ -13,13 +14,13 @@ export class MgModalService {
   // APP COMPONENT MUST HAVE
   // constructor(public viewContainerRef: ViewContainerRef)
 
-  create(component: any, data?: any, onClose?: () => undefined) {
-
-    console.log(`Creating ...`);
+  create(component: any, data?: any, onClose?: () => any) {
+    this._onClose = onClose;
 
     let cf = this.componentFactoryResolver.resolveComponentFactory(component);
     this._viewContainerRef = (this.appRef.components[0].instance as AppComponent).viewContainerRef;
-    this.viewContainerRef.clear();
+    // this.viewContainerRef.clear();
+    this.destroy();
     this._active = this.viewContainerRef.createComponent(cf);
 
     if (data) {
@@ -31,12 +32,15 @@ export class MgModalService {
 
   destroy() {
     // let cf = this.componentFactoryResolver.resolveComponentFactory(component);
-    this._viewContainerRef = (this.appRef.components[0].instance as AppComponent).viewContainerRef;
+    // this._viewContainerRef = (this.appRef.components[0].instance as AppComponent).viewContainerRef;
 
     if (this.viewContainerRef) {
       this.viewContainerRef.clear();
     }
     this._active = undefined;
+    if(this._onClose) {
+      this._onClose();
+    }
   }
 
   private get viewContainerRef() {
